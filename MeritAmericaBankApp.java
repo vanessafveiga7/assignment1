cad6268d01d528b8270d94be7d91d10ddd289035
Joey Sanchez <joeyrb.sanchez@gmail.com>
2021-04-26 11:02:54 -0700

    First commit

--- MeritAmericaBankApp.java
+++ MeritAmericaBankApp.java
@@ -5,11 +5,15 @@ public class MeritAmericaBankApp {
 		AccountHolder acHolder = new AccountHolder("John", "James", "Doe", "1123456789", 100.00, 1000.00);
 		AccountHolder acHolder1 = new AccountHolder("Jane", "Mary", "Doe", "1124468256", 200.00, 500.00);
 		
-		
-		System.out.println(acHolder.toString());//display the account holder's toString() output
+		// Display the account holder's toString() output
+		System.out.println(acHolder.toString());
 		acHolder.getCheckingAccount().deposit(500.00);
 		acHolder.getSavingsAccount().withdraw(800.00);
+		
+		// Display the checking account's toString() ouput
 		System.out.println(acHolder.getCheckingAccount().toString());
+		
+		// Display the savings account's toString() output
 		System.out.println(acHolder.getSavingsAccount().toString());
 		acHolder1.getCheckingAccount().deposit(-500.00);
 		acHolder1.getSavingsAccount().withdraw(600.00);
